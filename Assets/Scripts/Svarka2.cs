cd27c4e1d6b4e49f6f3d40d7b1473e2137ebead0
EvgeniyRSB <podolskiye@mail.ru>
2021-05-17 09:21:18 +0300

    second commit

---
+++ Assets/Scripts/Svarka2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Svarka2 : MonoBehaviour
+{
+    public Transform sparks_clone;
+    public Transform Sparks;
+    public GameObject Weapon;
+    public GameObject Detal1;
+    public GameObject Detal2;
+    public GameObject Mod;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Svarka")
+        {
+            sparks_clone = Instantiate(Sparks, transform.position, Quaternion.identity);
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Svarka")
+        {
+            Vector3 gun_pos = Weapon.transform.position;
+            Weapon.SetActive(false);
+            Detal1.SetActive(false);
+            Detal2.SetActive(false);
+            Mod.transform.position = new Vector3(Weapon.transform.position.x, Weapon.transform.position.y, Weapon.transform.position.z);
+            Mod.transform.rotation.Set(360, -358, 150, Mod.transform.rotation.w);
+            Destroy(Weapon);
+            Destroy(Detal1);
+            Destroy(Detal2);
+            Destroy(sparks_clone.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SvarkaTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SvarkaTrigger : MonoBehaviour
+{
+    public Transform sparks_clone;
+    public Transform Sparks;
+    public GameObject Gun;
+    public GameObject Detal;
+    public GameObject Model;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Svarka")
+        {
+            sparks_clone = Instantiate(Sparks, transform.position, Quaternion.identity);
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Svarka")
+        {
+
+            Vector3 gun_pos = Gun.transform.position;
+            Gun.SetActive(false);
+            Detal.SetActive(false);
+            Model.transform.position = new Vector3(Gun.transform.position.x, Gun.transform.position.y, Gun.transform.position.z);
+            Model.transform.rotation.Set(360, -358, 150, Model.transform.rotation.w);
+            Destroy(Gun);
+            Destroy(Detal);
+            Destroy(sparks_clone.gameObject);       
+        }
+    }
+}
