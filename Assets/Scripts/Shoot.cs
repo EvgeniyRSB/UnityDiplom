ee961ad3f86dd2913b4b7681498cab297ba6fc49
EvgeniyRSB <podolskiye@mail.ru>
2021-05-20 12:21:12 +0300

    6 commit

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject muzzleFlashPrefab;
+    [SerializeField] private Transform barrelLocation;
+
+    [Header("Settings")]
+    [Tooltip("Specify time to destory the casing object")] [SerializeField] private float destroyTimer = 2f;
+    [Tooltip("Bullet Speed")] [SerializeField] private float shotPower = 500f;
+    [Tooltip("Casing Ejection Speed")] [SerializeField] private float ejectPower = 150f;
+
+    void Fire()
+    {
+        if (muzzleFlashPrefab)
+        {
+            GameObject tempFlash;
+            tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
+            Destroy(tempFlash, destroyTimer);
+        }
+
+        if (!bulletPrefab)
+        { return; }
+        Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation).GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
+    }
+}
+
